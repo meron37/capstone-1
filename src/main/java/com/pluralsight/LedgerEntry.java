30288f6c8d15bd1201621b5054ffe039eb00feb9
meron37 <134938986+meron37@users.noreply.github.com>
2025-10-14 09:33:45 -0500

    I have added getter and setter

--- src/main/java/com/pluralsight/LedgerEntry.java
+++ src/main/java/com/pluralsight/LedgerEntry.java
@@ -1,17 +1,60 @@
 package com.pluralsight;
 
 public class LedgerEntry {
-        private String date;
-        private String description;
-        private String vendor;
-        private double amount;
-        private String type; // "deposit" or "debit"
+    private String date;
+    private String description;
+    private String vendor;
+    private double amount;
+    private String type; // "deposit" or "debit"
 
-        // Constructor
-        public LedgerEntry(String date, String description, String vendor, double amount, String type) {
-            this.date = date;
-            this.description = description;
-            this.vendor = vendor;
-            this.amount = amount;
-            this.type = type;
-        }}
+    // Constructor
+    public LedgerEntry(String date, String description, String vendor, double amount, String type) {
+        this.date = date;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    // Getters
+    public String getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    // Setters
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
